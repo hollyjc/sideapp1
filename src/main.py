4300177f42410f563ca5de7ec3b7fb830bf65e49
Holly <holly_cooper@live.com.au>
2020-11-24 11:39:05 +1000

    main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@
 import unittest
 import os
 import re
-
+from flask import Flask
+from flask import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask import Marshmallow
 
 def add(a, b):
     #add function is a dummy test
     return (a + b)
+
