8b8ffd72ddd85d1479325e13fca606a549df8243
Holly <holly_cooper@live.com.au>
2020-11-24 11:48:16 +1000

    flask stuff

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import unittest
 import os
 import re
 from flask import Flask
-from flask import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask import Marshmallow
 
@@ -12,3 +12,16 @@ def add(a, b):
     #add function is a dummy test
     return (a + b)
 
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("default_settings.app_config")
+
+    if app.config["ENV"] == "production":
+        from log_handlers import file_handler
+        app.logger.addHandler(file_handler)
